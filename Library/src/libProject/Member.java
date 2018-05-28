9a33a3894c647c373983d51dbc51dc7441418221
paeroo <paeroo0304@naver.com>
2018-05-28 17:41:27 +0900

    멤버 수정 최신

--- Library/src/libProject/Member.java
+++ Library/src/libProject/Member.java
@@ -1,12 +1,12 @@
 package libProject;
 
 public class Member implements Data{
-	String id;
-	String password;
-	String name;
-	String ssn;
-	String tel;
-	boolean status;
+	private String id;
+	private String password;
+	private String name;
+	private String ssn;
+	private String tel;
+	private boolean status;
 	
 		
 	public Member(String id, String password, String name, String ssn, String tel) {
@@ -15,6 +15,7 @@ public class Member implements Data{
 		this.name = name;
 		this.ssn = ssn;
 		this.tel = tel;
+		
 	}
 	void IdStatus(boolean status) {
 		if(status == true) {
