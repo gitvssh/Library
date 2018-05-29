4e74e52cd4f0783f96f15d1ef170e4d9b6c662ca
Administrator <Administrator@sinchon-e-PC>
2018-05-29 12:31:48 +0900

    id만 받는 검색추가

--- Library/src/libProject/AdminDB.java
+++ Library/src/libProject/AdminDB.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class AdminDB extends DB {
-//2018.05.28 18:10pm.
+	// 2018.05.28 18:10pm.
 	List<Admin> adminList;
 
 	Data input() {
@@ -13,10 +13,8 @@ public class AdminDB extends DB {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("새로운 관리자를 등록합니다.");
 		System.out.println();
-
 		System.out.println("아이디를 입력하세요.");
 		String id = sc.nextLine();
-
 		System.out.println("패스워드를 입력하세요.");
 		String password = sc.nextLine();
 		while (true) { // 패스워드 확인하는 구문
@@ -46,19 +44,14 @@ public class AdminDB extends DB {
 		adminList.add((Admin) admin);
 	}
 
-	List searchid(String id) {//검색 : id 
+	@Override
+	List search(String id) {
 		List<Admin> searchId = new ArrayList<Admin>();
-		for(Admin a:adminList) {
-			if(a.getName().contains(id)) searchId.add(a);
+		for (Admin a : adminList) {
+			if (a.getName().contains(id))
+				searchId.add(a);
 		}
 		return searchId;
-	}
-	List searchname(String name) {//검색 : 이름
-		List<Admin> searchName = new ArrayList<Admin>();
-		for(Admin a:adminList) {
-			if(a.getName().contains(name)) searchName.add(a);
-		}
-		return searchName;
 	}
 
 	@Override
@@ -89,12 +82,6 @@ public class AdminDB extends DB {
 
 	@Override
 	List align() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	List search(String title) {
 		// TODO Auto-generated method stub
 		return null;
 	}
